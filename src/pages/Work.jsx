4996920daad663ceb4cfba652228921c3474a1ad
Olaitan01 <abiodunfatunbi4@gmail.com>
2026-05-28 21:12:57 +0100

    fixed: Broken contact me button

--- src/pages/Work.jsx
+++ src/pages/Work.jsx
@@ -224,7 +224,7 @@ export default function Work() {
                     </div>
 
                     <NavLink
-                      href="./Contact"
+                      to="./Contact"
                       className="w-fit mt-6 md:mt-0 border border-black px-6 py-3 rounded-full text-xs tracking-[0.2em] uppercase transition-all duration-300 hover:bg-black hover:text-white"
                       style={{
                         fontFamily: "’Roboto Mono’, monospace",
